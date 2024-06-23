82f14ac9ab2206f30c1b4d12ecbd7b86570b3776
halal-beef <78730004+halal-beef@users.noreply.github.com>
2024-06-23 12:20:29 +0100

    misc refinements

--- build.sh
+++ build.sh
@@ -6,7 +6,3 @@ set -e
 ./build_bootshim.sh
 GCC5_AARCH64_PREFIX=aarch64-linux-gnu- build -j$(nproc) -s -n 0 -a AARCH64 -t GCC5 -p EXYNOS7885Pkg/Devices/a10.dsc
 cat BootShim/BootShim.bin workspace/Build/EXYNOS7885Pkg/DEBUG_GCC5/FV/EXYNOS7885PKG_UEFI.fd > workspace/UEFI
-mkbootimg --kernel workspace/UEFI -o workspace/boot.img
-cd workspace/
-tar -c boot.img -f boot.tar
-cd ..
